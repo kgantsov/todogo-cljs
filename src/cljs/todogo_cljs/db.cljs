d1666e625f3364831935872c3004cae65b1c6364
Kostiantyn Hantsov <k.gantsov@gmail.com>
2017-07-25 15:42:35 +0200

    Run everything in docker using docker-compose and nginx as a frontend

--- src/cljs/todogo_cljs/db.cljs
+++ src/cljs/todogo_cljs/db.cljs
@@ -17,23 +17,23 @@
 
 
 (defn get-todo-lists []
-  (GET "http://localhost:8080/api/v1/list/"
+  (GET "/api/v1/list/"
        {:handler (fn [data] (dispatch [:set-todo-lists (keywordize-keys data)]))}))
 
 (defn get-todo-list [list-id]
-  (GET (str "http://localhost:8080/api/v1/list/" list-id "/")
+  (GET (str "/api/v1/list/" list-id "/")
        {:handler (fn [data] (dispatch [:set-todo-list (keywordize-keys data)]))}))
 
 (defn get-todos [id]
-  (GET (str "http://localhost:8080/api/v1/list/" id "/todo/")
+  (GET (str "/api/v1/list/" id "/todo/")
        {:handler (fn [data] (dispatch [:set-todos (keywordize-keys data)]))}))
 
 (defn get-todo [list-id id]
-  (GET (str "http://localhost:8080/api/v1/list/" list-id "/todo/" id "/")
+  (GET (str "/api/v1/list/" list-id "/todo/" id "/")
        {:handler (fn [data] (dispatch [:set-todo (keywordize-keys data)]))}))
 
 (defn create-todo-list [ data]
-  (POST (str "http://localhost:8080/api/v1/list/")
+  (POST (str "/api/v1/list/")
         {:format        :json
          :params        data
          :handler       (fn [] (do (dispatch [:set-todo-list-title ""])
@@ -41,7 +41,7 @@
          :error-handler (fn [r] (prn r))}))
 
 (defn create-todo [list-id data]
-  (POST (str "http://localhost:8080/api/v1/list/" list-id "/todo/")
+  (POST (str "/api/v1/list/" list-id "/todo/")
         {:format        :json
          :params        data
          :handler       (fn [] (do (dispatch [:set-todo-title ""])
@@ -49,7 +49,7 @@
          :error-handler (fn [r] (prn r))}))
 
 (defn toggle-todo [todo]
-  (PUT (str "http://localhost:8080/api/v1/list/" (:todo_list_id todo) "/todo/" (:id todo) "/")
+  (PUT (str "/api/v1/list/" (:todo_list_id todo) "/todo/" (:id todo) "/")
         {:format        :json
          :params        {:title (:title todo)
                          :completed (if (= (:completed todo) true) false true)
@@ -58,16 +58,16 @@
          :error-handler (fn [r] (prn r))}))
 
 (defn update-todo [todo]
-  (PUT (str "http://localhost:8080/api/v1/list/" (:todo_list_id todo) "/todo/" (:id todo) "/")
+  (PUT (str "/api/v1/list/" (:todo_list_id todo) "/todo/" (:id todo) "/")
         {:format        :json
          :params        todo
          :handler       (fn [] (get-todos (:todo_list_id todo)))
          :error-handler (fn [r] (prn r))}))
 
 (defn delete-todo-list [todo-list-id]
-  (DELETE (str "http://localhost:8080/api/v1/list/" todo-list-id "/")
+  (DELETE (str "/api/v1/list/" todo-list-id "/")
           {:handler (fn [] (get-todo-lists))}))
 
 (defn delete-todo [todo-list-id todo-id]
-  (DELETE (str "http://localhost:8080/api/v1/list/" todo-list-id "/todo/" todo-id "/")
+  (DELETE (str "/api/v1/list/" todo-list-id "/todo/" todo-id "/")
           {:handler (fn [] (get-todos todo-list-id))}))
