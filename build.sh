87f0127d254be1f37fb915480f72b8694658f631
Kostiantyn Hantsov <k.gantsov@gmail.com>
2017-08-29 18:20:30 +0200

    Add `build.sh` script to build docker container and push to `hub.docker.com`

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+NAME="todogo_cljs"
+TAG=latest
+USER="kgantsov"
+DOCKER_ID_USER="kgantsov"
+
+rm -Rf resources/public/js/compiled
+lein cljsbuild once min
+lein less once
+
+docker build -f Dockerfile-prod -t $USER/$NAME:$TAG --no-cache .
+
+
+docker tag $USER/$NAME:$TAG $USER/$NAME:$TAG
+docker push $USER/$NAME:$TAG
+
+
+docker rmi $USER/$NAME:$TAG
