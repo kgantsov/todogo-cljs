ea6d18ba4965446545bd84b3f6da7970fcde5c60
Kostiantyn Hantsov <k.gantsov@gmail.com>
2017-11-09 21:10:24 +0100

    Changed footer

--- src/cljs/todogo_cljs/components.cljs
+++ src/cljs/todogo_cljs/components.cljs
@@ -48,7 +48,7 @@
 (def footer
   [:footer {:class "footer"}
    [:div {:class "container"}
-    [:p {:class "text-muted"} "© 2016 Company, Inc."]]])
+    [:p {:class "text-muted"} "© 2016 TODOGO"]]])
 
 
 (defn bread-crumbs [crumbs]
